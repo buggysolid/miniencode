6a1ac6ee0fda459a668a44e8bb14cb1c75af1de1
Ciaran <106989304+buggysolid@users.noreply.github.com>
2022-09-26 11:38:44 +0100

    Update encode.sh
    Adjust encoding to use CBR of 3600k and force audio to 2 channel aac.

--- encode.sh
+++ encode.sh
@@ -20,6 +20,6 @@ fi
 FFMPEG_TARGET_DIRECTORY="$(tar -tf ffmpeg.tar.xz | head -n 1)"
 OUTPUT_FILE_NAME="$1.720p.x265.mkv"
 
-FFMPEG_ENCODE_COMMAND="$FFMPEG_TARGET_DIRECTORY/ffmpeg -i $INPUT -vf scale=1280:720 -c:v libx265 -crf 28 -maxrate 3500k -bufsize 8096k -preset $PRESET -pass 1 -ac 2 -c:a aac -c:b 128k $OUTPUT_FILE_NAME"
+FFMPEG_ENCODE_COMMAND="$FFMPEG_TARGET_DIRECTORY/ffmpeg -i $INPUT -vf scale=1280:720 -c:v libx265 -minrate 3600k -maxrate 3600k -bufsize 8096k -preset slow -pass 1 -ac 2 -c:a aac -ab 128k -tag:v hvc1 $OUTPUT_FILE_NAME"
 
 $($FFMPEG_ENCODE_COMMAND)
